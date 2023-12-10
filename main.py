f8324bbfb7b00ecbf2b6cb777cb8f314a63ca414
Premraj Paul <94243441+Premraj12345@users.noreply.github.com>
2023-12-10 17:31:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, render_template, request
 from time import sleep
 
@@ -25,7 +24,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://whatsapp.checkleaked.cc/380947100983")  # Specify the URL you want to navigate to
 
 # Wait for the element to be present on the page - to load fully
-WebDriverWait(driver, 10).until(
+WebDriverWait(driver, 50).until(
     EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/div/div'))
 )
 
@@ -38,16 +37,33 @@ def index():
 def get_data():
     phone_number = request.args.get('phone_number')
 
+    form_element = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/form')  # Real ID of your form
     phone_number_input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/form/div[1]/div[2]/div/div/div[3]/input')
-    phone_number_input.clear()
-    phone_number_input.send_keys(str(phone_number))
+
+    #phone_number_input.click()
+
+    # Double-click on the phone number input element
+    ActionChains(driver).double_click(phone_number_input).perform()
+
+    # Use backspace to delete the last character
+    for sp in range(10):
+        ActionChains(driver).send_keys(Keys.BACKSPACE).perform()
+
+    for digit in phone_number:
+        ActionChains(driver).send_keys(digit).perform()
+        # (Optional) Add a small delay between key presses
+        #driver.implicitly_wait(0.1)
+
+    #phone_number_input.send_keys(phone_number)
+    #form_element.submit()
+
 
     # Find the button element by its type attribute
     button_element = driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div/div/form/div[2]/button")
     button_element.click()
 
     # Wait for the element to be present on the page
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/div/div'))
     )
 
